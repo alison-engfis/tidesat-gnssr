11bca68f1461658181f751cec3b9c8b443dd142a
Alison Soares <alison.engfis@gmail.com>
2024-09-27 21:40:12 -0300

    Código fonte .py para o streamlit app

--- nivel_st.py
+++ nivel_st.py
@@ -2,40 +2,40 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-@st.cache  # Cache para evitar recarregamento dos dados em cada interação
 
+@st.cache_data # Cache para evitar recarregamento dos dados em cada interação
 def load_data():
     
     # Carregar os dados (ajustando o necessário)
+    df = pd.read_csv('sph4_out.csv', sep=',')
 
-    df = pd.read_csv('sph4_out.csv', sep= ',')
+    df.rename(columns={'% year': 'year', ' month': 'month', ' day': 'day', ' hour': 'hour', ' minute': 'minute', ' second (GMT/UTC)': 'second',
+                       ' water level (meters)': 'water_level(m)'}, inplace= True)
 
-    df.rename(columns= {'% year' : 'year', ' month' : 'month', ' day' : 'day', ' hour' : 'hour', ' minute' : 'minute', ' second (GMT/UTC)' : 'second', 
-                        ' water level (meters)' : 'water_level(m)'}, inplace= True)
-    
-    df['datetime'] = pd.to_datetime(df[['year', 'month', 'day', 'hour', 'minute', 'second']])
+    df['datetime'] = pd.to_datetime(
+        df[['year', 'month', 'day', 'hour', 'minute', 'second']])
 
     return df
 
 # Título
-st.title("Monitoramento do Nível do Rio Guaíba")
+st.title("Monitoramento do Nível do Guaíba")
 
-# Carregar os dados
+# Dados
 data = load_data()
 
 # Tabela contendo os dados
-st.write("Dados de Nível de Água:")
+st.write("Dados de Nível:")
 st.dataframe(data[['datetime', 'water_level(m)']])
 
 # Plot
 st.write("Gráfico do Nível de Água ao Longo do Tempo:")
-fig, ax = plt.subplots(figsize= (12, 6))
+fig, ax = plt.subplots(figsize= (14, 6))
 ax.plot(data['datetime'], data['water_level(m)'], linestyle= '-')
-ax.set_xlabel('Data e Hora')
-ax.set_ylabel('Nível da Água (metros)')
-ax.set_title('Nível do Rio Guaíba ao Longo do Tempo')
+ax.set_xlabel('Data')
+ax.set_ylabel('Nível (m)')
+ax.set_title('Nível do Guaíba ao Longo do Tempo')
 plt.xticks(rotation= 45)
 plt.grid(True)
 
-# Gráfico no Streamlit
+# Gráfico no Streamlit app
 st.pyplot(fig)
