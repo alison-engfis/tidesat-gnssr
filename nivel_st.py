aca1c53cf98388ba55f0fe04f00bc687c205eb94
Alison Soares <alison.engfis@gmail.com>
2024-09-27 23:01:21 -0300

    Update nivel_st.py (adicionando requests)

--- nivel_st.py
+++ nivel_st.py
@@ -1,41 +1,62 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+import requests
+from io import StringIO
 
+# URL do arquivo CSV externo
+url = "https://app.tidesatglobal.com/sph4/sph4_out.csv"
 
-@st.cache_data # Cache para evitar recarregamento dos dados em cada interação
+# Função para carregar os dados do link externo
+@st.cache_data(ttl= 600)  # Cache por t = 10 min
 def load_data():
     
-    # Carregar os dados (ajustando o necessário)
-    df = pd.read_csv('sph4_out.csv', sep=',')
+    # Teste de request
+    response = requests.get(url, verify= False, timeout= 10)
+    
+    if response.status_code == 200:
 
-    df.rename(columns={'% year': 'year', ' month': 'month', ' day': 'day', ' hour': 'hour', ' minute': 'minute', ' second (GMT/UTC)': 'second',
-                       ' water level (meters)': 'water_level(m)'}, inplace= True)
+        dados_nivel = StringIO(response.text)
+        df = pd.read_csv(dados_nivel, sep= ',')
+        
+        df.rename(columns={'% year': 'year', ' month': 'month', ' day': 'day', ' hour': 'hour', 
+                           ' minute': 'minute', ' second (GMT/UTC)': 'second',
+                           ' water level (meters)': 'water_level(m)'}, inplace= True)
 
-    df['datetime'] = pd.to_datetime(
-        df[['year', 'month', 'day', 'hour', 'minute', 'second']])
+        df['datetime'] = pd.to_datetime(df[['year', 'month', 'day', 'hour', 'minute', 'second']])
 
-    return df
+        return df
+    else:
+        st.error("Erro ao acessar os dados.")
+        return None
 
 # Título
 st.title("Monitoramento do Nível do Guaíba")
 
-# Dados
-data = load_data()
+# Carregar os dados
+dados = load_data()
 
-# Tabela contendo os dados
-st.write("Dados de Nível:")
-st.dataframe(data[['datetime', 'water_level(m)']])
+# Verificando se os dados foram carregados com êxito
+if dados is not None:
 
-# Plot
-st.write("Gráfico do Nível de Água ao Longo do Tempo:")
-fig, ax = plt.subplots(figsize= (14, 6))
-ax.plot(data['datetime'], data['water_level(m)'], linestyle= '-')
-ax.set_xlabel('Data')
-ax.set_ylabel('Nível (m)')
-ax.set_title('Nível do Guaíba ao Longo do Tempo')
-plt.xticks(rotation= 45)
-plt.grid(True)
+    # Tabela de dados
+    st.write("Dados de Nível:")
+    st.dataframe(dados[['datetime', 'water_level(m)']])
+
+    # Plot
+    st.write("Gráfico do Nível da Água ao Longo do Tempo:")
+    fig, ax = plt.subplots(figsize= (14, 6))
+    ax.plot(dados['datetime'], dados['water_level(m)'], linestyle= '-')
+    ax.set_xlabel('Data')
+    ax.set_ylabel('Nível (m)')
+    ax.set_title('Nível do Guaíba ao Longo do Tempo')
+    plt.xticks(rotation=45)
+    plt.grid(True)
+
+    # Gráfico no app Streamlit
+    st.pyplot(fig)
+else:
+    st.write("Falha ao carregar os dados.")
 
 # Gráfico no Streamlit app
 st.pyplot(fig)
