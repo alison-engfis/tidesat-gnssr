b99f6acbe75977e120129a5f4bd41174316fd73d
Alison Soares <alison.engfis@gmail.com>
2024-10-01 23:42:53 -0300

    Teste de interatividade

--- nivel_st.py
+++ nivel_st.py
@@ -1,25 +1,21 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
 import requests
 from io import StringIO
+import plotly.express as px
 
 # URL do arquivo CSV com os dados
 url = "https://app.tidesatglobal.com/sph4/sph4_out.csv"
 
-# st.cache_data.clear()
-
-# Função para carregar os dados do link externo
-@st.cache_data(ttl=600)  # Cache por t = 10 min
+# Função para carregar os dados do link
+@st.cache_data(ttl= 600)  # Cache por t = 10 min
 def load_data():
-    
     # Teste de request
     response = requests.get(url, verify= False, timeout= 10)
     
     if response.status_code == 200:
-
         dados_nivel = StringIO(response.text)
-        df = pd.read_csv(dados_nivel, sep= ',')
+        df = pd.read_csv(dados_nivel, sep=',')
         
         df.rename(columns= {'% year': 'year', ' month': 'month', ' day': 'day', ' hour': 'hour', 
                            ' minute': 'minute', ' second (GMT/UTC)': 'second',
@@ -32,30 +28,31 @@ def load_data():
         st.error("Erro ao acessar os dados.")
         return None
 
+# Logo TideSat
+st.image("logo_empresa.png", width= 200)
+
 # Título
-st.title("Monitoramento do Nível do Guaíba")
+st.markdown("<h1 style= 'text-align: center; font-size: 30px;'>Monitore o nível da água onde quiser,acesse de onde estiver!</h1>", unsafe_allow_html= True)
 
 # Carregar os dados
 dados = load_data()
 
 # Verificando se os dados foram carregados com êxito
 if dados is not None:
+    
+    # Plot interativo
+    fig = px.line(dados_filtrados, x= 'datetime', y= 'water_level(m)', title= 'Dados da estação (SPH4) - Guaíba')
+    fig.update_xaxes(title_text= 'Data')
+    fig.update_yaxes(title_text= 'Nível (m)')
 
-    # Tabela de dados
-    # st.write("Dados de Nível:")
-    # st.dataframe(dados[['datetime', 'water_level(m)']])
+    # Tamanho das fontes
+    fig.update_layout(
+        title= {'font': {'size': 25}, 'x': 0.5, 'xanchor': 'center'},
+        xaxis_title= {'font': {'size': 20}},
+        yaxis_title= {'font': {'size': 20}},
+        font= {'size': 18})
 
-    # Plot
-    st.write("Gráfico do Nível da Água ao Longo do Tempo:")
-    fig, ax = plt.subplots(figsize= (14, 6))
-    ax.plot(dados['datetime'], dados['water_level(m)'], linestyle= '-')
-    ax.set_xlabel('Data')
-    ax.set_ylabel('Nível (m)')
-    ax.set_title('Nível do Guaíba ao Longo do Tempo')
-    plt.xticks(rotation= 45)
-    plt.grid(True)
-
-    # Gráfico no app Streamlit
-    st.pyplot(fig)
+    # Gráfico interativo no Streamlit app
+    st.plotly_chart(fig, use_container_width= True)
 else:
     st.write("Falha ao carregar os dados.")
