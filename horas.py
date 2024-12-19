4c2e5f83498caf75fc121b3698840cfdcb2af2bb
Alison Soares <alison.engfis@gmail.com>
2024-12-19 00:55:38 -0300

    Resolvendo o cabeçalho

--- horas.py
+++ horas.py
@@ -3,8 +3,18 @@ import pandas as pd
 import os
 import plotly.express as px
 
-# Configuração da página
-st.set_page_config(page_title="Registro de Horas", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
+## Configuração da página
+st.set_page_config(
+    page_title="Registro de Horas", 
+    layout="centered", 
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': None,  # Remove o item de ajuda
+        'About': None,     # Remove o item "Sobre"
+        'Report a bug': None,  # Remove a opção "Reportar um bug"
+        'Streamlit Cloud': None  # Remove o ícone do GitHub/Streamlit Cloud
+    }
+)
 
 # Reduz a margem superior da página
 st.markdown(
@@ -16,16 +26,6 @@ st.markdown(
     </style>
     """,
     unsafe_allow_html=True)
-
-''' Ocultando menu e rodapé (via CSS)
-esconder = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        .stActionButton {display: none;}
-        </style>
-        """
-st.markdown(esconder, unsafe_allow_html=True)'''
 
 # Arquivo de dados
 DATA_FILE = "registro_horas.csv"
